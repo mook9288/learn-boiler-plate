69b5f758c6946c985ab565b32a0dd4709f6f84d0
mook <mook9288@gmail.com>
2021-12-06 15:02:31 +0900

    mongoDB 연결하기

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://mook:test1234@inflearn01.r9udb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    // mongoose의 버전이 6 이상부터는 아래 코드 오류!!!
+    // {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopologe: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false,
+    // }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((error) => console.log(error));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
