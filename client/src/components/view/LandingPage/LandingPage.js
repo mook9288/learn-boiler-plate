6eb9ef57d3b4cf84efe8272d5fbb6643ad5bb9f7
mook <mook9288@gmail.com>
2021-12-14 15:48:49 +0900

    Add 로그아웃

--- client/src/components/view/LandingPage/LandingPage.js
+++ client/src/components/view/LandingPage/LandingPage.js
@@ -1,11 +1,46 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get('/api/hello').then((response) => console.log(response.data));
   }, []);
-  return <div>LandingPage</div>;
+
+  const onClickHanndler = () => {
+    axios.get('/api/users/logout').then((response) => {
+      if (response.data.success) {
+        props.history.push('/login');
+      } else {
+        alert('로그아웃 하는데 실패 했습니다.');
+      }
+    });
+  };
+  return (
+    <Layout>
+      <p>LandingPage</p>
+      <button onClick={onClickHanndler}>로그아웃</button>
+    </Layout>
+  );
 }
+
+const Layout = styled.div`
+  margin: 0 auto;
+  width: 180px;
+
+  p {
+    text-align: center;
+    font-weight: bold;
+    font-size: 20px;
+  }
+
+  button {
+    margin-top: 30px;
+    width: 100%;
+    height: 30px;
+    line-height: 28px;
+    border: 1px solid #333;
+  }
+`;
 
 export default LandingPage;
