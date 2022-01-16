7d14ef8341efe6870329697860234d98a2f7f801
mook <mook9288@gmail.com>
2022-01-16 17:20:49 +0900

    bugfix: fix typo

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
-const port = process.env.PROT || 5000;
+const port = process.env.PORT || 5000;
 const config = require('./config/key');
 const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
